5949fcdd1b873ab2993220ef50eee0d16e549e50
Marcos Moura <marcosmoura201@hotmail.com>
2023-03-10 08:52:17 -0300

    Iniciando projeto para salvar dados com Spring

---
+++ src/main/java/com/mmpCoder/Vendasappi/VendasAppiApplication.java
@@ -0,0 +1,13 @@
+package com.mmpCoder.Vendasappi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasAppiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasAppiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmpCoder/Vendasappi/VendasAppiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmpCoder.Vendasappi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasAppiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
